a14a3ece32497e82a2ca71c9311a08dbc574e0aa
Timur Tairov <tairovtk@gmail.com>
2025-01-09 19:27:36 +0300

    commit 1.13

--- components/BottomImg.jsx
+++ components/BottomImg.jsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import img from '@/images/bottom6.png'
+import img1 from '@/images/vector.svg'
 const BottomImg = () => {
   return (
-    <div className="bg-base-200 pt-10 md:pt-14 lg:pt-16 xl:pt-20">
+    <div className="bg-base-200">
+      <div className="flex justify-end">
+        <Image src={img1} alt="spiral" className="w-1/2 opacity-50 z-0" />
+      </div>
       <Image src={img} alt="img" className="w-full" />
     </div>
   )
